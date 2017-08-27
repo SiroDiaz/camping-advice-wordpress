4c03f188ea3cf2a8dffff384f54ddc8bdf5b19d7
DigbyCeaser <4f7baea1@opayq.com>
2017-08-27 22:51:59 +0100

    update

---
+++ buddypress/buddypress.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<div class="row">
+index-buddy
+    <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <!--slider section-->
+        <div class="row">
+           <article id="main-content">
+
+ 
+<div class="entry-content">
+
+<?php the_content(); ?>
+
+</div>
+
+<!-- .entry-content -->
+
+
+           </article>
+    </div><!-- .row -->
+
+  </div><!-- .col -->
+
+</div><!-- .row -->
+
+ <?php get_sidebar('blog'); ?>
+ 
+<?php get_footer(); ?>
